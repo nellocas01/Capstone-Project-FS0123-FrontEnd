67e017ca2038010e83ac3bf494cb4057c6486d93
Nello Casolla <nellocasolla446@gmail.com>
2023-07-13 11:52:28 +0200

    implementazione ProfilePage dinamica

--- src/component/ProfilePage.jsx
+++ src/component/ProfilePage.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
-import FooterComponent from "./FooterComponent";
+import { Card, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
 import NavbarComponent from "./NavbarComponent";
+import {
+  Facebook,
+  Instagram,
+  PersonCircle,
+  Twitter,
+} from "react-bootstrap-icons";
 
 const ProfilePage = () => {
   const [userDetails, setUserDetails] = useState();
@@ -19,6 +24,7 @@ const ProfilePage = () => {
     if (token) {
       fetchUser();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
 
   const fetchUser = async () => {
@@ -50,74 +56,96 @@ const ProfilePage = () => {
       <NavbarComponent />
 
       {/* PROFILE PAGE */}
-      <Container className="my-5">
+      <section>
         <h2 className="d-flex justify-content-center my-5">I tuoi dati</h2>
-        <Form>
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-              Nome
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="text"
-                placeholder=""
-                value={userDetails?.name}
-              />
+        <Container>
+          <Row className="justify-content-center align-items-center h-100">
+            <Col lg={6} className="mb-4 mb-lg-0">
+              <Card className="mb-3" style={{ borderRadius: ".5rem" }}>
+                <Row className="g-0">
+                  <Col
+                    md="4"
+                    className="gradient-custom text-center text-white"
+                    style={{
+                      borderTopLeftRadius: ".5rem",
+                      borderBottomLeftRadius: ".5rem",
+                    }}
+                  >
+                    <div className="mt-5">
+                      <PersonCircle
+                        className="mb-5"
+                        style={{ fontSize: "80px" }}
+                      />
+                      <h4 className="my-4">{userDetails?.name}</h4>
+                      <h3 className="my-4">{userDetails?.surname}</h3>
+                    </div>
+                  </Col>
+                  <Col md="8">
+                    <Card.Body className="p-4">
+                      <h6>Calciatore</h6>
+                      <hr className="mt-0 mb-4" />
+                      <Row className="pt-1">
+                        <Col size="6" className="mb-3">
+                          <h6>Username</h6>
+                          <Card.Text className="text-muted">
+                            {userDetails?.username}
+                          </Card.Text>
+                        </Col>
+                        <Col size="6" className="mb-3">
+                          <h6>Ruolo</h6>
+                          <Form.Select defaultValue="default">
+                            <option value="default" disabled>
+                              Che ruolo sei?
+                            </option>
+                            <option value="portiere">Portiere</option>
+                            <option value="difensore">Difensore</option>
+                            <option value="centrocampista">
+                              Centrocampista
+                            </option>
+                            <option value="attaccante">Attaccante</option>
+                          </Form.Select>
+                        </Col>
+                      </Row>
+                      <h6 tag="h6">Info</h6>
+                      <hr className="mt-0 mb-4" />
+                      <Row className="pt-1">
+                        <Col size="6" className="mb-3">
+                          <h6>Email</h6>
+                          <Card.Text className="text-muted">
+                            {userDetails?.email}
+                          </Card.Text>
+                        </Col>
+                        <Col size="6" className="mb-3">
+                          <h6>Password</h6>
+                          <InputGroup>
+                            <Form.Control
+                              className="psw-text"
+                              type="password"
+                              defaultValue={userDetails?.password}
+                              readOnly
+                            />
+                          </InputGroup>
+                        </Col>
+                      </Row>
+                      <div className="d-flex justify-content-start">
+                        <a href="#!">
+                          <Facebook size={74} />
+                        </a>
+                        <a href="#!">
+                          <Twitter size={74} />
+                        </a>
+                        <a href="#!">
+                          <Instagram size={74} />
+                        </a>
+                      </div>
+                    </Card.Body>
+                  </Col>
+                </Row>
+              </Card>
             </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-              Cognome
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="text"
-                placeholder=""
-                value={userDetails?.surname}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-              Username
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="text"
-                placeholder=""
-                value={userDetails?.username}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-              Email
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="email"
-                placeholder="Email"
-                value={userDetails?.email}
-              />
-            </Col>
-          </Form.Group>
-          <Form.Group as={Row} className="mb-3">
-            <Form.Label column sm={2}>
-              Password
-            </Form.Label>
-            <Col sm={10}>
-              <Form.Control
-                type="password"
-                placeholder=""
-                value={userDetails?.password}
-              />
-            </Col>
-          </Form.Group>
-
-          {/* FOOTER*/}
-          <FooterComponent />
-        </Form>
-      </Container>
+          </Row>
+        </Container>
+      </section>
     </>
   );
 };
